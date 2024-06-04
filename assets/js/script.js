a6f51f362bb67cc688f11ccfd94e7028c78527f1
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-06-04 15:33:00 +0100

    Move right click restriction to images only SECURITY

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 /* Script to Prevent Right Click(image copying) */
 document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('contextmenu', function (e) {
-        e.preventDefault();
+    var images = document.querySelectorAll('img'); // Select all images
+
+    // Iterate over each image and attach an event listener
+    images.forEach(function (img) {
+        img.addEventListener('contextmenu', function (e) {
+            e.preventDefault(); // Prevent the context menu from appearing
+        });
     });
 });
