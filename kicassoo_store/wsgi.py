e2ca59948a40917f9f7a91a841ef532882fa8b75
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-06-21 12:02:57 +0100

    Change secret key, Hide env variables from repo

--- kicassoo_store/wsgi.py
+++ kicassoo_store/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+import dotenv
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kicassoo_store.settings')
 
