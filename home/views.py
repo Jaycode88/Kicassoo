0a642e43c259ab8aed24b10510fab95ab8230677
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-07-09 11:38:38 +0100

    Add About page and styling, Inc Pre styles for events

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ def index(request):
     """A view that displays the index page of the site."""
     
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
