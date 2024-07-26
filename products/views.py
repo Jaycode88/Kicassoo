523ea3d6816259ae8543c6847c63a8d9e1aec47f
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-07-26 15:27:01 +0100

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def product_list(request):
+    """ A view to show all products ,including sorting and search queries """
+
     products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('product_list'))
+
+            Queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(Queries)
+
+            # Handle empty search results
+            if not products.exists():
+                messages.warning(request, "Your search returned no results.")
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+
+    return render(request, 'products/product_list.html', context)
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
