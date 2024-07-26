a199762d4d9aa7ae78828885c1fcb8f8e1e0515c
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-07-26 15:06:32 +0100

    Add Category model to products app

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'printful_id', 'price', 'description')
+    list_display = ('name', 'printful_id', 'price', 'description', 'category')
     search_fields = ('name', 'printful_id')
-    list_filter = ('price',)
-    readonly_fields = ('printful_id',)  # Assuming you want to make printful_id read-only
+    list_filter = ('price', 'category')
+    readonly_fields = ('printful_id',)
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
