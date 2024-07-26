fea060afaaa5886fc6bf4bb4a94df2243f72a627
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-07-26 15:06:32 +0100

    Add Category model to products app

--- products/models.py
+++ products/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     printful_id = models.CharField(max_length=255, unique=True)
     image_url = models.URLField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
