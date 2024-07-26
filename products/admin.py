d52a9d0d91ae95fd15a1df080f67305945864d0a
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-07-26 14:27:52 +0100

    Add description to product model

--- products/admin.py
+++ products/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'printful_id', 'price', 'description')
+    search_fields = ('name', 'printful_id')
+    list_filter = ('price',)
+    readonly_fields = ('printful_id',)  # Assuming you want to make printful_id read-only
+
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     printful_id = models.CharField(max_length=255, unique=True)
     image_url = models.URLField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
