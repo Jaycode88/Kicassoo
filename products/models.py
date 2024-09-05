4854fb8d9083c5a41435611a1968f0a6cb152a0e
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-09-05 16:34:44 +0100

    Fixed Delivery charge pulled from API

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     printful_id = models.CharField(max_length=255, unique=True)
+    variant_id = models.CharField(max_length=255, unique=True)
     image_url = models.URLField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
