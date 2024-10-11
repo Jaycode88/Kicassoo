d1ee687fb0631a3a37075421b2f6cb4ae90621e4
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-10-11 13:09:07 +0100

    Add handle payment failed webhook view, template & url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 from .views import place_order, order_success
+from checkout import views as checkout_views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),  # The main checkout view
     path('calculate-delivery/', views.calculate_delivery, name='calculate_delivery'),  # The view to calculate delivery
     path('place-order/', place_order, name='place_order'),
     path('order-success/', order_success, name='order_success'),
-    path('stripe/webhook/', views.stripe_webhook, name='stripe_webhook'),
+    path('checkout/payment-failed/', checkout_views.payment_failed, name='payment_failed'),
+    
 ]
