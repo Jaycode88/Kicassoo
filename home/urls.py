76c04b31808233ead675199b4faca8d35af2237b
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-11-06 20:33:51 +0000

    Add collection pages with views and urls, Add Collection images

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -7,4 +9,11 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('events/', views.events, name='events'),
+    path('collections/all/', views.all_collections, name='all_collections'),
+    path('collections/perfectmoments/', views.perfectmoments, name='perfectmoments'),
+    path('collections/ropesofwisdom/', views.ropesofwisdom, name='ropesofwisdom'),
+    path('collections/thekingdom/', views.thekingdom, name='thekingdom'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -8,5 +8,17 @@ def index(request):
 def about(request):
     return render(request, 'home/about.html')
 
+def all_collections(request):
+    return render(request, 'home/collections/all_collections.html')
+
+def perfectmoments(request):
+    return render(request, 'home/collections/perfectmoments.html')
+
+def ropesofwisdom(request):
+    return render(request, 'home/collections/ropesofwisdom.html')
+
+def thekingdom(request):
+    return render(request, 'home/collections/thekingdom.html')
+
 def events(request):
     return render(request, 'home/events.html')
