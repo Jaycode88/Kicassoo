3943ef421061d56a47325c955fb7cc8bda9fa99f
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-12-14 14:31:28 +0000

    Modify product details display

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,21 @@ from .models import Product, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'printful_id', 'price', 'description', 'category')
+    list_display = ('name', 'printful_id', 'price', 'category', 'intro_description', 'details')
     search_fields = ('name', 'printful_id')
     list_filter = ('price', 'category')
     readonly_fields = ('printful_id',)
+    fields = (
+        'name', 
+        'printful_id', 
+        'variant_id', 
+        'sync_variant_id', 
+        'image_url', 
+        'price', 
+        'description',
+        'category', 
+        'size'
+    )
 
 
 admin.site.register(Product, ProductAdmin)
